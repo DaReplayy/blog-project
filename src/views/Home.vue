329bd02d67a062996dcc3c86697bf1e6ad90e325
Logan Kovash <l.kovash@yahoo.com>
2021-08-05 07:19:33 -0600

    updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,26 +1,28 @@
 <template>
   <div class="home">
-    <p>my name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">Click Me</button>
+    <h2>Home</h2>
+    <PostList v-if="showPosts" :posts="posts"/>
+    <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">remove a post</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
+import { ref } from 'vue'
+import PostList from '../components/PostList.vue'
 
 export default {
+  components: { PostList },
   name: 'Home',
   setup() {
+    const posts = ref([
+      { title: 'Welcome to the blog', body: 'Im baby raw denim trust fund intelligentsia keffiyeh shabby chic everyday carry blue bottle pour-over lo-fi succulents. Pabst occupy yr 8-bit selvage meditation XOXO lomo quinoa fashion axe readymade asymmetrical blog farm-to-table. Seitan heirloom live-edge, you probably havent heard of them affogato 90s flannel lumbersexual bicycle rights raw denim scenester tote bag pork belly thundercats salvia. Williamsburg crucifix iPhone, raw denim marfa helvetica truffaut man braid try-hard keytar tattooed. Shoreditch raclette post-ironic, banjo brunch intelligentsia locavore venmo prism cloud bread bitters microdosing disrupt 90s kale chips. Truffaut poke squid vaporware, vexillologist chillwave green juice mumblecore pug franzen flexitarian. Typewriter vexillologist celiac, tousled pour-over chambray activated charcoal 90s la croix swag mlkshk freegan migas trust fund +1.', id: 1 },
 
-    let name = 'mario'
-    let age = 30
-
-    const handleClick = () => {
-      console.log('You clicked me')
-    }
-
-    return{ name, age, handleClick }
+      { title: 'Top 5 CSS Tips', body: 'Im baby raw denim trust fund intelligentsia keffiyeh shabby chic everyday carry blue bottle pour-over lo-fi succulents. Pabst occupy yr 8-bit selvage meditation XOXO lomo quinoa fashion axe readymade asymmetrical blog farm-to-table. Seitan heirloom live-edge, you probably havent heard of them affogato 90s flannel lumbersexual bicycle rights raw denim scenester tote bag pork belly thundercats salvia. Williamsburg crucifix iPhone, raw denim marfa helvetica truffaut man braid try-hard keytar tattooed. Shoreditch raclette post-ironic, banjo brunch intelligentsia locavore venmo prism cloud bread bitters microdosing disrupt 90s kale chips. Truffaut poke squid vaporware, vexillologist chillwave green juice mumblecore pug franzen flexitarian. Typewriter vexillologist celiac, tousled pour-over chambray activated charcoal 90s la croix swag mlkshk freegan migas trust fund +1.', id: 2 }
+    ])
+    const showPosts = ref(true)
+    
+    return{ posts, showPosts }
   }
   
 }
